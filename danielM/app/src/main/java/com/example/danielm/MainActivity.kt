81644062a1701e1f00ccf2f6035e20a2a4393821
Safago96 <115754331+Safago96@users.noreply.github.com>
2024-03-06 18:25:30 -0500

    Add files via upload

---
+++ danielM/app/src/main/java/com/example/danielm/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.danielm
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
+
+
+class MainActivity : AppCompatActivity() {
+
+}
